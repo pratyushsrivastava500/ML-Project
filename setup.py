e5211668fb65a4201ba712369fd48dcf91f2f84d
pratyushsrivastava500 <PRATYUSHSRIVASTAVA500@GMAIL.com>
2025-02-04 21:33:38 +0530

    third commit

--- setup.py
+++ setup.py
@@ -14,10 +14,6 @@ def get_requirements(file_path:str)->List[str]:
             requirements.remove(HYPEN_E_DOT)
     return requirements
 
-
-
-
-
 setup(
     name="Projects",
     version="0.0.1",
